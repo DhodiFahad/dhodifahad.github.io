f6c4fe704a6a860306231a157ca205691a3b7750
Kinene <fahad.marascientific.org>
2022-06-09 20:24:09 +0300

    some configs i don't understand

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const packageJSON = require('./package.json');
+const transpiledPackages = Object.keys(packageJSON.dependencies).filter(it => it.includes('@shared/'));
+
+const withTM = require('next-transpile-modules')(transpiledPackages);
+
+module.exports = withPlugins([withTM], {
+  // ...
+});
 const nextConfig = {
   reactStrictMode: true,
   images:{
