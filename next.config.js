5982e33ca6ba2b7f55e8b00ff5101f9d097d1eb8
Kinene <fahad.marascientific.org>
2022-06-09 19:31:11 +0300

    updated git actions configs

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
   images:{
     loader:"akamai",
     path:""
-  }
+  },
+  basePath:"/dhodifahad.github.io",
+  assetPrefix:"/dhodifahad.github.io"
 }
 
 module.exports = nextConfig
